0a047cbc090285817c33e3cc704a5c76eae4c932
chris turner <mr.chris.turner@gmail.com>
2018-07-19 21:50:59 -0700

    jimp

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var ba64 = require("ba64");
 var fs = require("fs");
+var request = require('request');
+var jimp = require('jimp');
+
+
+
 
 var app = express(); 
 
@@ -26,49 +31,48 @@ app.post('/choices/submit', function(request, response){
 	// console.log(request.body.picked_style)
 	var picked_style = request.body.picked_style;
 	var selfie = request.body.taken_selfie;
-	var data_url = "data:image/jpeg;base64" + selfie;
+	var data_url = "data:image/jpg;base64" + selfie;
 	// var data_url = "data:image/jpeg;base64, request.body.taken_selfie";
 
-  console.log(data_url),
+  // console.log(data_url),
 
 	// data_url = ba64.getBa64Img(data_url),
 	ba64.writeImage("myimage", data_url, function(err){
   		if (err) throw err;
 		  console.log("Image saved successfully");
-		  // do stuff
+
+		  deepDream.sendSelfie();
 		})
 });
 
+// JIMP to turn photo black and white. 
+
+
+var deepDream = {
+  sendSelfie: function() {
+    request.post({
+      url: 'https://api.deepai.org/api/deepdream',
+      headers: {
+          'Api-Key': '96c94bdc-77e0-4371-8a0d-8538d4c2693d'
+      },
+      formData: {
+          'content': fs.createReadStream('public/assets/styles/daydream-alphonse-mucha.jpg'),
+          // 'content': fs.createReadStream('myimage.jpg'),
+      }
+    }, function callback(err, httpResponse, body) {
+      if (err) {
+          console.error('request failed:', err);
+          return;
+      }
+      var response = JSON.parse(body);
+      console.log(response);
+    });
+  }
+
+}
 
 
 
-// var deepDream = {
-//   sendSelfie: function (selfie, style) {
-//     var fs = require('fs');
-//     var request = require('request');
-//     request.post({
-//       url: 'https://api.deepai.org/api/neural-style',
-//       headers: {
-//           'Api-Key': '96c94bdc-77e0-4371-8a0d-8538d4c2693d'
-//       },
-//       formData: {
-//           'style': fs.createReadStream('style'),
-//           'content': fs.createReadStream('selfie'),
-//       }
-//     }, function callback(err, httpResponse, body) {
-//       if (err) {
-//           console.error('request failed:', err);
-//           return;
-//       }
-//       var response = JSON.parse(body);
-//       console.log(response);
-//     });
-//   }
-
-// }
-
-
-// deepDream.sendSelfie();
 
 
 // listen for requests :)
