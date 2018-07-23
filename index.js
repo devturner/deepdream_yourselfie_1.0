7b5dc7971e327757276154331f1bed38d5550c7c
chris turner <mr.chris.turner@gmail.com>
2018-07-23 14:47:20 -0700

    trying to get this in the limits of heroku.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var ba64 = require("ba64");
 var fs = require("fs");
-
+var jimp = require("jimp")
 var app = express();
 
 //view engine
@@ -16,60 +16,60 @@ app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
 app.use(express.static('public'));
 
 
-// keep alive
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
+// // keep alive
+// const extendTimeoutMiddleware = (req, res, next) => {
+//   const space = ' ';
+//   let isFinished = false;
+//   let isDataSent = false;
 
-  // Only extend the timeout for API requests
-  if (!req.url.includes('/api')) {
-    next();
-    return;
-  }
+//   // Only extend the timeout for API requests
+//   if (!req.url.includes('/api')) {
+//     next();
+//     return;
+//   }
 
-  res.once('finish', () => {
-    isFinished = true;
-  });
+//   res.once('finish', () => {
+//     isFinished = true;
+//   });
 
-  res.once('end', () => {
-    isFinished = true;
-  });
+//   res.once('end', () => {
+//     isFinished = true;
+//   });
 
-  res.once('close', () => {
-    isFinished = true;
-  });
+//   res.once('close', () => {
+//     isFinished = true;
+//   });
 
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
+//   res.on('data', (data) => {
+//     // Look for something other than our blank space to indicate that real
+//     // data is now being sent back to the client.
+//     if (data !== space) {
+//       isDataSent = true;
+//     }
+//   });
 
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
+//   const waitAndSend = () => {
+//     setTimeout(() => {
+//       // If the response hasn't finished and hasn't sent any data back....
+//       if (!isFinished && !isDataSent) {
+//         // Need to write the status code/headers if they haven't been sent yet.
+//         if (!res.headersSent) {
+//           res.writeHead(202);
+//         }
 
-        res.write(space);
+//         res.write(space);
 
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
+//         // Wait another 15 seconds
+//         waitAndSend();
+//       }
+//     }, 15000);
+//   };
 
-  waitAndSend();
-  next();
-};
+//   waitAndSend();
+//   next();
+// };
 
-app.use(extendTimeoutMiddleware);
+// app.use(extendTimeoutMiddleware);
 
 
 
@@ -86,30 +86,66 @@ app.post('/results', function(request, response){
   
   ba64.writeImage("myimage", data_url, function(err){
   	if (err) throw err;
-	  console.log("Image saved successfully");
-  
-    var request = require("request");
-    request.post({
-      url: 'https://api.deepai.org/api/deepdream',
-      headers: {
-          'Api-Key': '96c94bdc-77e0-4371-8a0d-8538d4c2693d'
-      },
-      formData: {
-          'content': fs.createReadStream('myimage.jpg'),
-      }
-    }, function callback(err, httpResponse, body) {
-      if (err) {
-          console.error('request failed:', err);
-          return;
-      }
-      var reply = JSON.parse(body);
-      var redirect = reply.output_url;
-      var str = '<img id="deepSelfie" src="' + redirect + '">';
-      console.log(str);
-      response.render('results', {result: str});
-    });
-  })
+
+  // jimp.read("myimage.jpg", function(err, image){
+  //   if (err) throw err;
+  //     image.quality(45);
+  //     image.write("myimage1.jpg");
+
+    console.log("Image saved successfully");
+
+      
+      var request = require("request");
+      
+      request.post({
+        url: 'https://api.deepai.org/api/deepdream',
+        headers: {
+            'Api-Key': '96c94bdc-77e0-4371-8a0d-8538d4c2693d'
+        },
+        formData: {
+            'content': fs.createReadStream('myimage.jpg'),
+            // console.log(content)
+        }
+      }, function callback(err, httpResponse, body) {
+        if (err) {
+            console.error('request failed:', err);
+            return;
+        }
+        // console.log(JSON.parse(body));
+        var reply = JSON.parse(body);
+        var redirect = reply.output_url;
+        var str = '<img id="deepSelfie" src="' + redirect + '">';
+        console.log(str);
+        response.render('results', {result: str});
+      });
+    })
+// })
 });
+
+// function resize() {
+//   jimp.read("myimage.jpg", function(err, image) {
+//     if (err) { 
+//       throw err; 
+//     } else {
+//         image.quality(45);
+//         image.write("myimage.jpg");
+//         console.log("image quality lowered");
+//     }
+//   });
+// };
+// }
+  // function resize() {
+  //   jimp.read("myimage.jpg").then(function(image) {
+
+  //   return image.quality(45);
+  //          image.write("myimage.jpg");
+  //          console.log('happened')
+  //          console.log("image quality lowered");
+  //    }).catch(function (err) {
+  //      console.log('err');
+  //   });
+  // }
+
 
 
 // listen for requests :)
